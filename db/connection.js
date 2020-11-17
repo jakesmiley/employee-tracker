5516d631a07aec0d8a9f72772717969d11776c29
Jake Smiley <jake.alan.smiley@gmail.com>
2020-11-16 20:37:38 -0700

    minor fix

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,5 @@
 const mysql = require("mysql");
-const connection = mysql.createConnection({
+const connectionProperties = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
